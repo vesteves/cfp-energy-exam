b4a6c0fe81bda07aeea91973b1efab80ad6c8958
Vitor Esteves <vitordasilvaesteves@gmail.com>
2024-04-12 12:45:17 -0300

    feat: cleaning setup trashes

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,8 +4,6 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
-import './globals.css';
-
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
